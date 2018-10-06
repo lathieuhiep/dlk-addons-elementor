9dab91da209a5637b3d2206b5459046b0b10f405
KhacDiepKMA <khacdiepkma90@gmail.com>
2018-10-06 09:44:58 +0700

    edit  function check get category

--- includes/addons-elementor.php
+++ includes/addons-elementor.php
@@ -53,7 +53,12 @@ new dlk_addons_elementor_widgets();
 function dlk_check_get_cat( $type_taxonomy ) {
 
     $cat_check    =   array();
-    $category     =   get_categories( array( 'taxonomy'   =>  $type_taxonomy ) );
+    $category     =   get_terms(
+        array(
+            'taxonomy'      =>  $type_taxonomy,
+            'hide_empty'    =>  false
+        )
+    );
 
     if ( isset( $category ) && !empty( $category ) ):
 
