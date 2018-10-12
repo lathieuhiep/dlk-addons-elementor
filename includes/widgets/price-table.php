0843085fb3ad896fced44e18a399f5c20de624b0
KhacDiep <khacdiepkma90@gmail.com>
2018-10-12 17:39:38 +0700

    update code price table

--- includes/widgets/price-table.php
+++ includes/widgets/price-table.php
@@ -82,6 +82,187 @@ class Widget_DLK_Price_Table extends Widget_Base {
 
         $this->end_controls_section();
 
+        /* Section Price */
+        $this->start_controls_section(
+            'section_price',
+            [
+                'label' =>  esc_html__( 'Price', 'dlk-addons-elementor' )
+            ]
+        );
+
+        $this->add_control(
+            'price',
+            [
+                'label'         =>  esc_html__( 'Price', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  99,
+                'label_block'   =>  false
+            ]
+        );
+
+        $this->add_control(
+            'on_sale',
+            [
+                'type'          =>  Controls_Manager::SWITCHER,
+                'label'         =>  esc_html__('On Sale ?', 'dlk-addons-elementor'),
+                'label_on'      =>  esc_html__('Yes', 'dlk-addons-elementor'),
+                'label_off'     =>  esc_html__('No', 'dlk-addons-elementor'),
+                'return_value'  =>  'yes',
+                'default'       =>  'no',
+            ]
+        );
+
+        $this->add_control(
+            'sale_price',
+            [
+                'label'         =>  esc_html__( 'Sale Price', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  89,
+                'label_block'   =>  false,
+                'conditions'    =>  [
+                    'terms' =>  [
+                        [
+                            'name'  =>  'on_sale',
+                            'value' =>  'yes',
+                        ],
+                    ]
+                ]
+            ]
+        );
+
+        $this->add_control(
+            'price_currency',
+            [
+                'label'         =>  esc_html__( 'Price Currency', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  '$',
+                'label_block'   =>  false
+            ]
+        );
+
+        $this->add_control(
+            'currency_placement',
+            [
+                'label'     =>  esc_html__( 'Currency Placement', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::SELECT,
+                'default'   =>  'left',
+                'options'   =>  [
+                    'left'  =>  esc_html__( 'Left', 'dlk-addons-elementor' ),
+                    'right' =>  esc_html__( 'Right', 'dlk-addons-elementor' ),
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'price_period',
+            [
+                'label'         =>  esc_html__( 'Price Period (per)', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  esc_html__( 'month', 'dlk-addons-elementor' ),
+                'label_block'   =>  false
+            ]
+        );
+
+        $this->add_control(
+            'period_separator',
+            [
+                'label'         =>  esc_html__( 'Period Separator', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  '/',
+                'label_block'   =>  false
+            ]
+        );
+
+        $this->end_controls_section();
+
+        /* Section Feature */
+        $this->start_controls_section(
+            'section_feature',
+            [
+                'label' =>  esc_html__( 'Feature', 'dlk-addons-elementor' )
+            ]
+        );
+
+        $repeater = new Repeater();
+
+        $repeater->add_control(
+            'list_title', [
+                'label'         =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  esc_html__( 'Pricing table list item' , 'dlk-addons-elementor' ),
+                'label_block'   =>  true,
+            ]
+        );
+
+        $repeater->add_control(
+            'list_icon', [
+                'label'     =>  esc_html__( 'List Icon', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::ICON,
+                'default'   => 'fa fa-check',
+            ]
+        );
+
+        $repeater->add_control(
+            'item_active',
+            [
+                'type'          =>  Controls_Manager::SWITCHER,
+                'label'         =>  esc_html__('Item Active ?', 'dlk-addons-elementor'),
+                'label_on'      =>  esc_html__('Yes', 'dlk-addons-elementor'),
+                'label_off'     =>  esc_html__('No', 'dlk-addons-elementor'),
+                'return_value'  =>  'yes',
+                'default'       =>  'yes',
+            ]
+        );
+
+        $repeater->add_control(
+            'icon_color',
+            [
+                'label'     =>  esc_html__( 'Icon Color', 'dlk-addons-elementorn' ),
+                'type'      =>  Controls_Manager::COLOR,
+                'selectors' =>  [
+                    '{{WRAPPER}} {{CURRENT_ITEM}}' => 'color: {{VALUE}}'
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'list',
+            [
+                'label'     =>  '',
+                'type'      =>  Controls_Manager::REPEATER,
+                'fields'    =>  $repeater->get_controls(),
+                'default'   =>  [
+                    [
+                        'list_title'    =>  esc_html__( 'Unlimited calls', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                    [
+                        'list_title'    =>  esc_html__( 'Free hosting', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                    [
+                        'list_title'    =>  esc_html__( '500 MB of storage space', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                    [
+                        'list_title'    =>  esc_html__( '500 MB Bandwidth', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                    [
+                        'list_title'    =>  esc_html__( '24/7 support', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                    [
+                        'list_title'    =>  esc_html__( 'Pricing table list item', 'dlk-addons-elementorn' ),
+                        'list_icon'     =>  'fa fa-check',
+                    ],
+                ],
+                'title_field' => '{{{ list_title }}}',
+            ]
+        );
+
+        $this->end_controls_section();
+
         /* Section style title */
         $this->start_controls_section(
             'section_style_title',
@@ -159,7 +340,121 @@ class Widget_DLK_Price_Table extends Widget_Base {
 
     protected function render() {
 
-        $settings       =   $this->get_settings_for_display();
+        $settings           =   $this->get_settings_for_display();
+        $class_item_active  =   '';
+
+    ?>
+
+        <div class="element-price-table <?php echo esc_attr( $settings['style-1'] ); ?>">
+            <div class="element-price-table__item">
+                <h2 class="title">
+                    <?php echo esc_html( $settings['title'] ); ?>
+                </h2>
+
+                <div class="element-price-table__tag">
+                    <span class="price-tag">
+
+                        <?php
+                        if ( $settings['on_sale'] != 'yes' ) :
+
+                            if ( $settings['currency_placement'] == 'left' ) :
+                        ?>
+
+                            <span class="price-currency">
+                                <?php echo esc_html( $settings['price_currency'] ); ?>
+                            </span>
+
+                        <?php
+                            endif;
+
+                            echo esc_html( $settings['price'] );
+
+                            if ( $settings['currency_placement'] == 'right' ) :
+                        ?>
+
+                            <span class="price-currency">
+                                <?php echo esc_html( $settings['price_currency'] ); ?>
+                            </span>
+
+                        <?php
+                            endif;
+
+                        else:
+
+                            if ( $settings['currency_placement'] == 'left' ) :
+                        ?>
+
+                            <del class="muted-price">
+                                <span class="muted-price-currency">
+                                    <?php echo esc_html( $settings['price_currency'] ); ?>
+                                </span>
+
+                                <?php echo esc_html( $settings['price'] ); ?>
+                            </del>
+
+                            <span class="price-currency">
+                                <?php echo esc_html( $settings['price_currency'] ); ?>
+                            </span>
+
+                        <?php
+                            echo esc_html( $settings['sale_price'] );
+
+                            else:
+                        ?>
+
+                            <del class="muted-price">
+                                 <?php echo esc_html( $settings['price'] ); ?>
+
+                                 <span class="muted-price-currency">
+                                    <?php echo esc_html( $settings['price_currency'] ); ?>
+                                </span>
+                            </del>
+
+                            <?php echo esc_html( $settings['sale_price'] ); ?>
+
+                            <span class="price-currency">
+                                <?php echo esc_html( $settings['price_currency'] ); ?>
+                            </span>
+
+                        <?php
+                            endif;
+
+                        endif;
+                        ?>
+
+                    </span>
+
+                    <span class="price-period">
+                        <?php echo esc_html( $settings['period_separator'] ) . '&nbsp;' . esc_html( $settings['price_period'] ); ?>
+                    </span>
+                </div>
+
+                <?php if ( $settings['list'] ) : ?>
+
+                    <div class="element-price-table__feature">
+
+                        <?php
+                        foreach ( $settings['list'] as $item ) :
+
+                            if ( $item['item_active'] != 'yes' ) :
+                                $class_item_active = ' disable-item';
+                            endif;
+                        ?>
+
+                            <div class="item-list-feature<?php echo esc_attr( $class_item_active ) ?>">
+                                <i class="elementor-repeater-item-<?php echo esc_attr( $item['_id'] ); ?> <?php echo esc_attr( $item['list_icon'] ); ?>"></i>
+                                <?php echo esc_html( $item['list_title'] ); ?>
+                            </div>
+
+                        <?php endforeach; ?>
+
+                    </div>
+
+                <?php endif; ?>
+            </div>
+        </div>
+
+    <?php
 
     }
 
