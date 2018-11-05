63b9d2dbfa78cc400320eb8b180fc2f1125abf2b
KhacDiepKMA <khacdiepkma90@gmail.com>
2018-11-05 22:43:19 +0700

    update products filter

---
+++ includes/widgets/products-filter.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Widget_DLK_Products_Filter extends Widget_Base {
+
+    public function get_categories() {
+        return array( 'dlk-addons-elementor' );
+    }
+
+    public function get_name() {
+        return 'dlk-products-filter';
+    }
+
+    public function get_title() {
+        return esc_html__( 'DLK Products Filter', 'dlk-addons-elementor' );
+    }
+
+    public function get_icon() {
+        return 'eicon-post';
+    }
+
+    protected function _register_controls() {
+
+        /* Section Query */
+        $this->start_controls_section(
+            'section_query',
+            [
+                'label' =>  esc_html__( 'Query', 'dlk-addons-elementor' )
+            ]
+        );
+
+        $this->add_control(
+            'title',
+            [
+                'label'         =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::TEXT,
+                'default'       =>  esc_html__( 'Post', 'dlk-addons-elementor' ),
+                'label_block'   =>  true
+            ]
+        );
+
+        $this->add_control(
+            'select_cat',
+            [
+                'label'         =>  esc_html__( 'Select Category Product', 'dlk-addons-elementor' ),
+                'type'          =>  Controls_Manager::SELECT2,
+                'options'       =>  dlk_check_get_cat( 'product_cat' ),
+                'multiple'      =>  true,
+                'label_block'   =>  true
+            ]
+        );
+
+        $this->add_control(
+            'limit',
+            [
+                'label'     =>  esc_html__( 'Number of Posts', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::NUMBER,
+                'default'   =>  6,
+                'min'       =>  1,
+                'max'       =>  100,
+                'step'      =>  1,
+            ]
+        );
+
+        $this->add_control(
+            'order_by',
+            [
+                'label'     =>  esc_html__( 'Order By', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::SELECT,
+                'default'   =>  'id',
+                'options'   =>  [
+                    'id'            =>  esc_html__( 'Post ID', 'dlk-addons-elementor' ),
+                    'title'         =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
+                    'date'          =>  esc_html__( 'Date', 'dlk-addons-elementor' ),
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'order',
+            [
+                'label'     =>  esc_html__( 'Order', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::SELECT,
+                'default'   =>  'ASC',
+                'options'   =>  [
+                    'ASC'   =>  esc_html__( 'Ascending', 'dlk-addons-elementor' ),
+                    'DESC'  =>  esc_html__( 'Descending', 'dlk-addons-elementor' ),
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        /* Section Layout */
+        $this->start_controls_section(
+            'section_layout',
+            [
+                'label' =>  esc_html__( 'Layout Settings', 'dlk-addons-elementor' )
+            ]
+        );
+
+        $this->add_control(
+            'column_number',
+            [
+                'label'     =>  esc_html__( 'Column', 'dlk-addons-elementor' ),
+                'type'      =>  Controls_Manager::SELECT,
+                'default'   =>  3,
+                'options'   =>  [
+                    4   =>  esc_html__( '4 Column', 'dlk-addons-elementor' ),
+                    3   =>  esc_html__( '3 Column', 'dlk-addons-elementor' ),
+                    2   =>  esc_html__( '2 Column', 'dlk-addons-elementor' ),
+                    1   =>  esc_html__( '1 Column', 'dlk-addons-elementor' ),
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            Group_Control_Image_Size::get_type(),
+            [
+                'name'      =>  'thumbnail',
+                'exclude'   =>  [ 'custom' ],
+                'default'   =>  'medium_large',
+            ]
+        );
+
+        $this->end_controls_section();
+
+    }
+
+    protected function render() {
+
+        $settings       =   $this->get_settings_for_display();
+        $cat_post       =   $settings['select_cat'];
+        $limit_post     =   $settings['limit'];
+        $order_by_post  =   $settings['order_by'];
+        $order_post     =   $settings['order'];
+
+        if ( !empty( $cat_post ) ) :
+
+            $post_type_arg = array(
+                'post_type'         =>  'post',
+                'posts_per_page'    =>  $limit_post,
+                'orderby'           =>  $order_by_post,
+                'order'             =>  $order_post,
+                'cat'               =>  $cat_post
+            );
+
+        else:
+
+            $post_type_arg = array(
+                'post_type'         =>  'post',
+                'posts_per_page'    =>  $limit_post,
+                'orderby'           =>  $order_by_post,
+                'order'             =>  $order_post
+            );
+
+        endif;
+
+        $post_type_query = new \ WP_Query( $post_type_arg );
+
+        if ( $post_type_query->have_posts() ) :
+
+        ?>
+
+
+
+        <?php
+
+        endif;
+    }
+
+    protected function _content_template() {}
+
+}
+
+Plugin::instance()->widgets_manager->register_widget_type( new Widget_DLK_Products_Filter );
