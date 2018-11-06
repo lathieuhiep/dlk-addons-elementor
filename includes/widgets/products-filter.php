eaa5f421142e5975fd3648012438ec4efb0464bd
KhacDiepKMA <khacdiepkma90@gmail.com>
2018-11-06 22:37:33 +0700

    add option product

--- includes/widgets/products-filter.php
+++ includes/widgets/products-filter.php
@@ -37,7 +37,7 @@ class Widget_DLK_Products_Filter extends Widget_Base {
             [
                 'label'         =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
                 'type'          =>  Controls_Manager::TEXT,
-                'default'       =>  esc_html__( 'Post', 'dlk-addons-elementor' ),
+                'default'       =>  esc_html__( 'FEATURED PRODUCTS', 'dlk-addons-elementor' ),
                 'label_block'   =>  true
             ]
         );
@@ -58,7 +58,7 @@ class Widget_DLK_Products_Filter extends Widget_Base {
             [
                 'label'     =>  esc_html__( 'Number of Posts', 'dlk-addons-elementor' ),
                 'type'      =>  Controls_Manager::NUMBER,
-                'default'   =>  6,
+                'default'   =>  8,
                 'min'       =>  1,
                 'max'       =>  100,
                 'step'      =>  1,
@@ -72,9 +72,9 @@ class Widget_DLK_Products_Filter extends Widget_Base {
                 'type'      =>  Controls_Manager::SELECT,
                 'default'   =>  'id',
                 'options'   =>  [
-                    'id'            =>  esc_html__( 'Post ID', 'dlk-addons-elementor' ),
-                    'title'         =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
-                    'date'          =>  esc_html__( 'Date', 'dlk-addons-elementor' ),
+                    'id'    =>  esc_html__( 'Product ID', 'dlk-addons-elementor' ),
+                    'title' =>  esc_html__( 'Title', 'dlk-addons-elementor' ),
+                    'date'  =>  esc_html__( 'Date', 'dlk-addons-elementor' ),
                 ],
             ]
         );
@@ -107,7 +107,7 @@ class Widget_DLK_Products_Filter extends Widget_Base {
             [
                 'label'     =>  esc_html__( 'Column', 'dlk-addons-elementor' ),
                 'type'      =>  Controls_Manager::SELECT,
-                'default'   =>  3,
+                'default'   =>  4,
                 'options'   =>  [
                     4   =>  esc_html__( '4 Column', 'dlk-addons-elementor' ),
                     3   =>  esc_html__( '3 Column', 'dlk-addons-elementor' ),
@@ -132,42 +132,87 @@ class Widget_DLK_Products_Filter extends Widget_Base {
 
     protected function render() {
 
-        $settings       =   $this->get_settings_for_display();
-        $cat_post       =   $settings['select_cat'];
-        $limit_post     =   $settings['limit'];
-        $order_by_post  =   $settings['order_by'];
-        $order_post     =   $settings['order'];
+        $settings   =   $this->get_settings_for_display();
+        $select_cat =   $settings['select_cat'];
+        $limit      =   $settings['limit'];
+        $order_by   =   $settings['order_by'];
+        $order      =   $settings['order'];
 
-        if ( !empty( $cat_post ) ) :
+        if ( !empty( $select_cat ) ) :
 
-            $post_type_arg = array(
-                'post_type'         =>  'post',
-                'posts_per_page'    =>  $limit_post,
-                'orderby'           =>  $order_by_post,
-                'order'             =>  $order_post,
-                'cat'               =>  $cat_post
+            $args = array(
+                'post_type'         =>  'product',
+                'posts_per_page'    =>  $limit,
+                'orderby'           =>  $order_by,
+                'order'             =>  $order,
+                'tax_query'         =>  array(
+                    array(
+                        'taxonomy'  =>  'product_cat',
+                        'field'     =>  'id',
+                        'terms'     =>  $select_cat,
+                    ),
+                ),
             );
 
         else:
 
-            $post_type_arg = array(
-                'post_type'         =>  'post',
-                'posts_per_page'    =>  $limit_post,
-                'orderby'           =>  $order_by_post,
-                'order'             =>  $order_post
+            $args = array(
+                'post_type'         =>  'product',
+                'posts_per_page'    =>  $limit,
+                'orderby'           =>  $order_by,
+                'order'             =>  $order,
             );
 
         endif;
 
-        $post_type_query = new \ WP_Query( $post_type_arg );
+        $query = new \ WP_Query( $args );
 
-        if ( $post_type_query->have_posts() ) :
+        if ( $query->have_posts() ) :
 
-        ?>
+    ?>
 
+        <div class="element-products-filter">
+            <?php if ( !empty( $settings['title'] ) ) : ?>
+                <h4 class="title">
+                    <?php echo esc_html( $settings['title'] ); ?>
+                </h4>
+            <?php endif; ?>
 
+            <div class="row">
+                <?php while ( $query->have_posts() ): $query->the_post(); ?>
 
-        <?php
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-<?php echo esc_attr( 12 / $settings['column_number'] ); ?>">
+                        <div class="item-product">
+                            <div class="item-thumbnail">
+                                <?php if ( has_post_thumbnail() ) : ?>
+
+                                    <img src="<?php echo esc_url( Group_Control_Image_Size::get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'thumbnail', $settings ) ); ?>" alt="<?php the_title(); ?>">
+
+                                <?php
+                                else:
+                                    $no_team_member_image  =   dlk_addons_elementor_path . 'assets/images/no-images.png';
+                                ?>
+
+                                    <img src="<?php echo esc_url( $no_team_member_image ) ?>" alt="<?php the_title(); ?>" />
+
+                                <?php endif; ?>
+                            </div>
+
+                            <h2 class="item-title">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+
+                            <?php woocommerce_template_loop_price(); ?>
+                        </div>
+                    </div>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+            </div>
+        </div>
+
+    <?php
 
         endif;
     }
